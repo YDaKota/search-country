35673ec48700450a3a96537e790503aa2fbf7fee
JulkaUlka <y.glushko@outlook.com>
2023-02-20 15:18:57 +0200

    added saerh func

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,30 @@
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import { useState } from 'react';
 
-export const SearchForm = () => {
+export const SearchForm = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = e => {
+    setQuery(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit(query);
+  };
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={handleSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select
+        defaultValue="DEFAULT"
+        onChange={handleChange}
+        aria-label="select"
+        name="region"
+        required
+      >
+        <option value="DEFAULT" disabled defaultValue="">
           Select a region and press enter
         </option>
         <option value="africa">Africa</option>
